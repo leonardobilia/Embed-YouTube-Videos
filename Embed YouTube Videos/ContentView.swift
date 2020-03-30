a96b6a2cf53f950002a94f8f8bdccfd7cf7fc292
Leonardo Bilia <leonardo.bilia@gmail.com>
2020-03-30 14:21:09 -0300

    Initial Commit

---
+++ Embed YouTube Videos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Embed YouTube Videos
+//
+//  Created by Leonardo Bilia on 30/03/20.
+//  Copyright © 2020 Leonardo Bilia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
